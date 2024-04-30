d84af05d2150ea78dbf732907763228a63156865
Nikolay <shlyapnikov.nik@mail.ru>
2024-04-30 21:24:12 +0300

    add profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Page} from "../shared/ui/Page/ui";
+import {Header} from "../widgets/header/header";
+
+const ProfilePage = () => {
+    return (
+        <Page>
+            <Header></Header>
+        </Page>
+    );
+};
+
+export default ProfilePage;
